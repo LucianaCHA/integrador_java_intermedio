ec270538c4a1eccea0de80a263cacb0996df156c
luciana <luciana@infraPC-0469>
2023-11-15 23:58:01 -0300

    First commit

---
+++ tp_integrador/src/main/java/tp_integrador/Main.java
@@ -0,0 +1,12 @@
+package tp_integrador;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Proyecto iniciado");
+
+	}
+
+}
