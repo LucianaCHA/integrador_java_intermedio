6bd30871a4e6bde778860c0a19f917a07dd07ef2
Luciana <lucianachamorro87@gmail.com>
2023-11-26 12:52:25 -0300

    Agregado Readme y ajuste en vista consola

--- tp_integrador/src/main/java/com/argentinaprograma/issuereport/MenuIntialization.java
+++ tp_integrador/src/main/java/com/argentinaprograma/issuereport/MenuIntialization.java
@@ -32,16 +32,16 @@ public class MenuIntialization {
 
     public void initializeMenu() {
 
-        System.out.println(MenuConstantes.MENSAJE_INICIO_MENU);
+        System.out.println("\u001B[38m"  + MenuConstantes.MENSAJE_INICIO_MENU);
     
         for (int i = 0; i < MenuConstantes.MENU_PRINCIPAL.length; i++) {
-            System.out.println((i + 1) + ". " + MenuConstantes.MENU_PRINCIPAL[i]);
+            System.out.println("\u001B[34m"  + (i + 1) + ". " + MenuConstantes.MENU_PRINCIPAL[i]);
         }
         try {
             int opcionElegida = scanner.nextInt();
             cargarMenu(opcionElegida);
         } catch (InputMismatchException e) {
-            System.out.println(MenuConstantes.MENSAJE_ERROR_NUMERO_INVALIDO);
+            System.out.println("\u001B[31m"+ MenuConstantes.MENSAJE_ERROR_NUMERO_INVALIDO);
             scanner.next();
             initializeMenu();
         }
@@ -53,44 +53,44 @@ public class MenuIntialization {
 
             case 1:
                 int dias = 0;
-                System.out.println(MenuConstantes.MENSAJE_INGRESE_DIAS);
+                System.out.println("\u001B[35m"+  MenuConstantes.MENSAJE_INGRESE_DIAS);
                 dias = scanner.nextInt();
                 String resultado =  tecnicoService.conMasIncidentesResueltosEnXDias(dias);
-                System.out.println(resultado);
+                System.out.println("\u001B[32m"  +  resultado);
                 menuConstinuarSalir();
             
             case 2:
-				System.out.println(MenuConstantes.MENSAJE_ELEGIR_ESPECIALIDAD);
+				System.out.println("\u001B[31m"+  MenuConstantes.MENSAJE_ELEGIR_ESPECIALIDAD);
 				
 				for (int i = 0; i < especialidades.size(); i++) {
-					System.out.println((i + 1) + ". " + especialidades.get(i).getNombre());
+					System.out.println("\u001B[34m"+(i + 1) + ". " + especialidades.get(i).getNombre());
 				}
 				int especialidadElegida = scanner.nextInt() - 1;
 
-				System.out.println(MenuConstantes.MENSAJE_INGRESE_DIAS);
+				System.out.println("\u001B[35m"+ MenuConstantes.MENSAJE_INGRESE_DIAS);
 				dias = scanner.nextInt();
 
 				resultado = tecnicoService.conMasIncidentesPorEspecialidad(dias, especialidadElegida);
-				System.out.println(resultado);
+				System.out.println("\u001B[32m"+ resultado);
                 menuConstinuarSalir();
             
                 case 3:
                     resultado = tecnicoService.tecnicoMasRapido();
-				    System.out.println(resultado);
+				    System.out.println("\u001B[32m"+  resultado);
                     menuConstinuarSalir();
                 case 4:
                     System.exit(0);
                     break;
 
             default:
-                System.out.println(MenuConstantes.MENSAJE_ERROR_OPCION_INVALIDA);
+                System.out.println("\u001B[31m"+ MenuConstantes.MENSAJE_ERROR_OPCION_INVALIDA);
                 initializeMenu();
                 break;
         }
     }
 
     public void menuConstinuarSalir () {
-        System.out.println(MenuConstantes.MENSAJE_CONTINUAR_SALIR);
+        System.out.println("\u001B[30m"+ MenuConstantes.MENSAJE_CONTINUAR_SALIR);
         try {
             int opcion = scanner.nextInt();
 
@@ -99,12 +99,12 @@ public class MenuIntialization {
                     initializeMenu();
                     break;
                 default:
-                    System.out.println(MenuConstantes.MENSAJE_DESPEDIDA);
+                    System.out.println("\u001B[37m"+ MenuConstantes.MENSAJE_DESPEDIDA);
                     System.exit(0);
                     break;
             }
         } catch (InputMismatchException e) {
-            System.out.println(MenuConstantes.MENSAJE_ERROR_NUMERO_INVALIDO);
+            System.out.println("\u001B[31m"+ MenuConstantes.MENSAJE_ERROR_NUMERO_INVALIDO);
             scanner.next();
             initializeMenu();
 
