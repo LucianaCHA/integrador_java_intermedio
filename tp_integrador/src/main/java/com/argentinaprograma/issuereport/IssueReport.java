1dae8e66716f7bc5f61f45e70a95acacc492fd95
Luciana <lucianachamorro87@gmail.com>
2023-11-26 11:17:14 -0300

    Refactor saco menu del main

--- tp_integrador/src/main/java/com/argentinaprograma/issuereport/IssueReport.java
+++ tp_integrador/src/main/java/com/argentinaprograma/issuereport/IssueReport.java
@@ -1,12 +1,6 @@
 package com.argentinaprograma.issuereport;
 
-import com.argentinaprograma.issuereport.constants.MenuConstantes;
-import com.argentinaprograma.issuereport.model.Especialidad;
-import com.argentinaprograma.issuereport.services.EspecialidadService;
 import com.argentinaprograma.issuereport.services.TecnicoService;
-
-import java.util.Scanner;
-import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,63 +15,16 @@ public class IssueReport {
 		ConfigurableApplicationContext context = SpringApplication.run(IssueReport.class, args);
 
         DataInitilization dataInitialization = context.getBean(DataInitilization.class);
-		TecnicoService tecnicoService = context.getBean(TecnicoService.class);
-		EspecialidadService especialidadService = context.getBean(EspecialidadService.class);
+		MenuIntialization menuIntialization = context.getBean(MenuIntialization.class);
 
-		List<Especialidad> especialidades = especialidadService.buscarTodos();
+		TecnicoService tecnicoService = context.getBean(TecnicoService.class);
+
+	
 		if(tecnicoService.buscarTodos().size() == 0){
 			dataInitialization.initializeData();
 		}
-
-		Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Elija una opción:");
-
-        for (int i = 0; i < MenuConstantes.MENU_PRINCIPAL.length; i++) {
-            System.out.println((i + 1) + ". " + MenuConstantes.MENU_PRINCIPAL[i]);
-        }
-        int opcionElegida = scanner.nextInt();
-
-        switch (opcionElegida) {
-            case 1:
-				int dias = 0;
-				System.out.println("Ingrese la cantidad de días");
-				dias = scanner.nextInt();
-                String resultado =  tecnicoService.conMasIncidentesResueltosEnXDias(dias);
-
-				System.out.println(resultado);
-
-                break;
-            case 2:
-				System.out.println("Elija una especialidad:");
-				
-				for (int i = 0; i < especialidades.size(); i++) {
-					System.out.println((i + 1) + ". " + especialidades.get(i).getNombre());
-				}
-				int especialidadElegida = scanner.nextInt() - 1;
-
-				System.out.println("Ingrese la cantidad de días");
-
-				dias = scanner.nextInt();
-
-				resultado = tecnicoService.conMasIncidentesPorEspecialidad(dias, especialidadElegida);
-
-				System.out.println(resultado);
-
-                break;
-            case 3:
-                resultado = tecnicoService.tecnicoMasRapido();
-				System.out.println(resultado);
-                break;
-            case 4:
-                System.exit(0);
-                break;
-			
-            default:
-                System.out.println("Opción no válida");
-                break;
-        }
-		scanner.close();
-	}
+		menuIntialization.initializeMenu();
+	
+	 }
 
 }
--- tp_integrador/src/main/java/com/argentinaprograma/issuereport/constants/MenuConstantes.java
+++ tp_integrador/src/main/java/com/argentinaprograma/issuereport/constants/MenuConstantes.java
@@ -2,12 +2,19 @@ package com.argentinaprograma.issuereport.constants;
 
 public class MenuConstantes {
 
-    public static final         String[] MENU_PRINCIPAL= {
+    public static final String MENSAJE_INICIO_MENU = "Elija una opción:";
+    public static final String MENSAJE_INGRESE_DIAS = "Ingrese la cantidad de días";
+    public static final String MENSAJE_ELEGIR_ESPECIALIDAD = "Elija una especialidad:";
+    public static final String MENSAJE_CONTINUAR_SALIR = "¿Desea realizar una nueva consulta? 1 para continuar, cualquier otro número para salir";
+    public static final String MENSAJE_DESPEDIDA = "Gracias por utilizar el sistema";
+    public static final String MENSAJE_ERROR_NUMERO_INVALIDO = "El número ingresado no es válido \n \n Regreso a menú principal ................";
+    public static final String MENSAJE_ERROR_OPCION_INVALIDA= "La opción ingresada no es válida \n \n Regreso a menú principal ................";
+
+    public static final String[] MENU_PRINCIPAL= {
         "Técnico con más incidentes resueltos en los últimos N días",
         "Técnico con más incidentes resueltos en los últimos N días por especialidad",
         "Técnico que resolvió más rápido los incidentes asignados",
         "Salir"
-
 
 };
 }
